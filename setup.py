ec7d714e17a8eb74bee3e89d63d0b9d92e0b91dc
akrherz <akrherz@iastate.edu>
2013-08-17 10:25:17 -0500

    move src/pyldm to pyldm/ move __version__ into pyldm/__init__.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 
+import pyldm
+
 setup(
     name='pyLDM',
-    version='0.0.1',
+    version=pyldm.__version__,
     author='daryl herzmann',
     author_email='akrherz@gmail.com',
     packages=['pyldm'],
     url='https://github.com/akrherz/pyLDM/',
-    package_dir={'pyldm':'src/pyldm'},
+    package_dir={'pyldm':'pyldm'},
     license='Apache',
     description="Python utilities to interface with Unidata's LDM.",
 )
