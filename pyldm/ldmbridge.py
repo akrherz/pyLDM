715630568d21022b7ec5622a4964ac3b790cfc3a
akrherz <akrherz@iastate.edu>
2014-05-20 10:04:55 -0500

    implement deduplication as an optional protocol flag, fixes #1

--- pyldm/ldmbridge.py
+++ pyldm/ldmbridge.py
@@ -3,17 +3,60 @@ Created on Jan 5, 2013
 
 @author: akrherz
 '''
+#stdlib
+import hashlib
+import datetime
+
+#twisted imports
 from twisted.internet import stdio
 from twisted.protocols import basic
+from twisted.internet import reactor
+from twisted.python import log
 
 class LDMProductReceiver(basic.LineReceiver):
     product_start = '\001'
     product_end = '\r\r\n\003'
 
-    def __init__(self):
+    def __init__(self, dedup=False):
         self.productBuffer = ""
         self.setRawMode()
         self.cbFunc = self.process_data
+        self.cache = {}
+        if dedup:
+            self.cbFunc = self.filter_product
+            reactor.callLater(90, self.clean_cache)
+
+    def clean_cache(self):
+        ''' Cull old cache every 90 seconds '''
+        threshold = datetime.datetime.utcnow() - datetime.timedelta(hours=4)
+        for m in self.cache.keys():
+            if self.cache[m] < threshold:
+                del(self.cache[m])
+        reactor.callLater(90, self.clean_cache)
+
+    def filter_product(self, buf):
+        ''' Filter the product (deduplication) '''
+        buf = buf.replace('\x1e', '').replace('\t', '')
+        lines = buf.split("\015\015\012")
+        # Trim trailing empty lines    
+        while len(lines) > 0 and lines[-1] == "":
+            lines.pop()
+        if len(lines) == 0:
+            log.msg("Whoa, filterProduct hit no lines?")
+            return
+        product = "\015\015\012".join( lines )
+        digest = hashlib.md5( product ).hexdigest()
+        #log.msg("Cache size is"+ str(len(self.cache.keys())) )
+        #log.msg("digest is"+ str(digest) )
+        #log.msg("Product Size"+ str(len(product)) )
+        #log.msg("line0:"+ lines[0] +":")
+        if self.cache.has_key(digest):
+            log.msg("DUP! %s" % (",".join(lines[1:5]),) )
+        else:
+            self.cache[ digest ] = datetime.datetime.utcnow()
+            #log.msg("process_data() called")
+            self.process_data( product + "\015\015\012")
+
 
     def rawDataReceived(self, data):
         """ callback for when raw data is received on the stdin buffer, this 
--- pyldm/tests/test_ldmbridge.py
+++ pyldm/tests/test_ldmbridge.py
@@ -5,6 +5,30 @@ from pyldm import ldmbridge
 
 class StdioFilesTests(ReactorBuilder):
 
+    def test_deduplicate(self):
+        reactor = self.buildReactor()
+
+        class MyProductIngestor(ldmbridge.LDMProductReceiver):
+            hits = 0
+                      
+            def process_data(self, buf):
+                self.hits = self.hits + 1
+        
+            def connectionLost(self, reason):
+                reactor.stop()
+        
+
+        f = file('../../testdata/twoprods.txt', "r")
+        
+        ingest = MyProductIngestor(dedup=True)
+        ldmbridge.LDMProductFactory( ingest, 
+                                     stdin=f.fileno(),
+                                     reactor=reactor )
+
+        self.runReactor(reactor)
+        self.assertEqual(ingest.hits, 1)
+
+
     def test_bridge(self):
         reactor = self.buildReactor()
 
