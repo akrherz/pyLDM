d77ec9d144110dc4ee4b4e36e19aee77ccde4a25
akrherz <akrherz@iastate.edu>
2017-09-02 20:34:18 -0500

    non-func address pylint issues

--- pyldm/ldmbridge.py
+++ pyldm/ldmbridge.py
@@ -1,45 +1,52 @@
-'''
+"""
 Created on Jan 5, 2013
 
 @author: akrherz
-'''
-#stdlib
+"""
+# stdlib
 import hashlib
 import datetime
 
-#twisted imports
+# twisted imports
 from twisted.internet import stdio
 from twisted.protocols import basic
 from twisted.internet import reactor
 from twisted.python import log
 
+
 class LDMProductReceiver(basic.LineReceiver):
+    """Our Protocol"""
     product_start = '\001'
     product_end = '\r\r\n\003'
 
     def __init__(self, dedup=False):
+        """Constructor
+
+        Params:
+          dedup (boolean): should we attempt to filter out duplicates
+        """
         self.productBuffer = ""
         self.setRawMode()
         self.cbFunc = self.process_data
         self.cache = {}
         if dedup:
             self.cbFunc = self.filter_product
-            reactor.callLater(90, self.clean_cache)
+            reactor.callLater(90, self.clean_cache)  # @UndefinedVariable
 
     def clean_cache(self):
-        ''' Cull old cache every 90 seconds '''
+        """Cull old cache every 90 seconds"""
         threshold = datetime.datetime.utcnow() - datetime.timedelta(hours=4)
-        for m in self.cache.keys():
-            if self.cache[m] < threshold:
-                del(self.cache[m])
-        reactor.callLater(90, self.clean_cache)
+        for digest in self.cache.keys():
+            if self.cache[digest] < threshold:
+                del self.cache[digest]
+        reactor.callLater(90, self.clean_cache)  # @UndefinedVariable
 
     def filter_product(self, original):
         """ Implement Deduplication
          - Attempt to account for all of the wild variations that can happen
-         with LDM plexing of NOAAPort, WeatherWire, TOC Socket Feed and 
+         with LDM plexing of NOAAPort, WeatherWire, TOC Socket Feed and
          whatever else may be happening
-         
+
         1. If the character \x1e happens, we ignore it.  SPC issue here
         2. If we find \x17, this is some weather wire thing, we ignore whatever
            comes after it.
@@ -47,62 +54,61 @@ class LDMProductReceiver(basic.LineReceiver):
         4. We convert tab characters to blank spaces, as one of NWSTG's systems
            does this already and is a source of duplicates
         5. Ignore first 11 bytes in MD5 computation
-         
+
         If Okay, we end up calling self.process_data() with clean data
-        
+
         """
         clean = original.replace('\x1e', '').replace('\t', '')
         if clean.find("\x17") > 0:
-            #log.msg("control-17 found, truncating...")
+            # log.msg("control-17 found, truncating...")
             clean = clean[:clean.find("\x17")]
-        #log.msg("buffer[:20] is : "+ repr(buf[:20]) )
-        #log.msg("buffer[-20:] is : "+ repr(buf[-20:]) )
+        # log.msg("buffer[:20] is : "+ repr(buf[:20]) )
+        # log.msg("buffer[-20:] is : "+ repr(buf[-20:]) )
         lines = clean.split("\015\015\012")
-        # Trim trailing empty lines    
-        while len(lines) > 0 and lines[-1].strip() == "":
+        # Trim trailing empty lines
+        while lines and lines[-1].strip() == "":
             lines.pop()
-        if len(lines) == 0:
+        if not lines:
             log.msg("ERROR: filter_product culled entire product (no data?)")
             return
         lines[1] = lines[1][:3]
-        clean = "\015\015\012".join( lines )
-        digest = hashlib.md5( clean[11:] ).hexdigest()
-        #log.msg("Cache size is : "+ str(len(self.cache.keys())) )
-        #log.msg("digest is     : "+ str(digest) )
-        #log.msg("Product Size  : "+ str(len(product)) )
-        #log.msg("len(lines)    : "+ str(len(lines)) )
-        if self.cache.has_key(digest):
-            log.msg("DUP! %s" % (",".join(lines[1:5]),) )
+        clean = "\015\015\012".join(lines)
+        digest = hashlib.md5(clean[11:]).hexdigest()
+        # log.msg("Cache size is : "+ str(len(self.cache.keys())) )
+        # log.msg("digest is     : "+ str(digest) )
+        # log.msg("Product Size  : "+ str(len(product)) )
+        # log.msg("len(lines)    : "+ str(len(lines)) )
+        if digest in self.cache:
+            log.msg("DUP! %s" % (",".join(lines[1:5]),))
         else:
-            self.cache[ digest ] = datetime.datetime.utcnow()
-            #log.msg("process_data() called")
-            self.process_data(clean +"\015\015\012")
-
+            self.cache[digest] = datetime.datetime.utcnow()
+            # log.msg("process_data() called")
+            self.process_data(clean + "\015\015\012")
 
     def rawDataReceived(self, data):
-        """ callback for when raw data is received on the stdin buffer, this 
+        """ callback for when raw data is received on the stdin buffer, this
         could be a partial product or lots of products """
         # See if we have anything left over from previous iteration
         if self.productBuffer != "":
             data = self.productBuffer + data
-        
+
         tokens = data.split(self.product_end)
         # If length tokens is 1, then we did not find the splitter
         if len(tokens) == 1:
-            #log.msg("Token not found, len data %s" % (len(data),))
+            # log.msg("Token not found, len data %s" % (len(data),))
             self.productBuffer = data
             return
 
-        # Everything up until the last one can always go...        
+        # Everything up until the last one can always go...
         for token in tokens[:-1]:
-            #log.msg("ldmbridge cb product size: %s" % (len(token),))
-            self.cbFunc( token )
+            # log.msg("ldmbridge cb product size: %s" % (len(token),))
+            self.cbFunc(token)
         # We have some cruft left over!
         if tokens[-1] != "":
             self.productBuffer = tokens[-1]
         else:
             self.productBuffer = ""
-                    
+
     def connectionLost(self, reason):
         raise NotImplementedError
 
@@ -113,12 +119,9 @@ class LDMProductReceiver(basic.LineReceiver):
         ''' needless override to make pylint happy '''
         pass
 
-class LDMProductFactory( stdio.StandardIO ):
+
+class LDMProductFactory(stdio.StandardIO):
 
     def __init__(self, protocol, **kwargs):
         """ constructor with a protocol instance """
         stdio.StandardIO.__init__(self, protocol, **kwargs)
-
-
-
-
