9f2f2f243759aa7164d50402b34f24e2f4df926d
akrherz <akrherz@iastate.edu>
2018-05-24 11:05:44 -0500

    first swing at proper python2+3 support

--- pyldm/ldmbridge.py
+++ pyldm/ldmbridge.py
@@ -1,47 +1,48 @@
-"""
-Created on Jan 5, 2013
-
-@author: akrherz
-"""
+"""A Twisted Python bridge for pqact exec'd processing"""
 # stdlib
 import hashlib
 import datetime
+from io import BytesIO
 
 # twisted imports
 from twisted.internet import stdio
 from twisted.protocols import basic
-from twisted.internet import reactor
 from twisted.python import log
 
 
 class LDMProductReceiver(basic.LineReceiver):
     """Our Protocol"""
-    product_start = '\001'
-    product_end = '\r\r\n\003'
+    reactor = None
+    product_start = b'\001'
+    product_end = b'\r\r\n\003'
 
     def __init__(self, dedup=False, isbinary=False):
         """Constructor
 
-        Params:
+        Args:
           dedup (boolean): should we attempt to filter out duplicates
-          isbinary (boolean): should we not attempt unicode decoding?
+          isbinary (boolean): should we not attempt bytes decoding
+            process_data will either return a string and bytes
         """
-        self.productBuffer = u""
+        self.bytes_received = 0
+        self.productBuffer = BytesIO()
+        # this puts twisted out of the pure line receiver mode
         self.setRawMode()
         self.cbFunc = self.process_data
         self.cache = {}
         self.isbinary = isbinary
-        if dedup:
+        self.dedup = dedup
+        if self.dedup:
             self.cbFunc = self.filter_product
-            reactor.callLater(90, self.clean_cache)  # @UndefinedVariable
 
     def clean_cache(self):
         """Cull old cache every 90 seconds"""
         threshold = datetime.datetime.utcnow() - datetime.timedelta(hours=4)
+        # loop safety with this
         for digest in self.cache.keys():
             if self.cache[digest] < threshold:
                 del self.cache[digest]
-        reactor.callLater(90, self.clean_cache)  # @UndefinedVariable
+        self.reactor.callLater(90, self.clean_cache)  # @UndefinedVariable
 
     def filter_product(self, original):
         """ Implement Deduplication
@@ -59,6 +60,8 @@ class LDMProductReceiver(basic.LineReceiver):
 
         If Okay, we end up calling self.process_data() with clean data
 
+        Args:
+          original (str): hopefully gaurenteed to be a string type
         """
         clean = original.replace('\x1e', '').replace('\t', '')
         if clean.find("\x17") > 0:
@@ -75,7 +78,8 @@ class LDMProductReceiver(basic.LineReceiver):
             return
         lines[1] = lines[1][:3]
         clean = "\015\015\012".join(lines)
-        # Our data is unicode and needs to be encoded prior to hashing
+        # first 11 characters should not be included in hex, like LDM does
+        # hashlib works on bytes
         digest = hashlib.md5(clean[11:].encode('utf-8')).hexdigest()
         # log.msg("Cache size is : "+ str(len(self.cache.keys())) )
         # log.msg("digest is     : "+ str(digest) )
@@ -92,46 +96,52 @@ class LDMProductReceiver(basic.LineReceiver):
         """callback from twisted when raw data is received
 
         Args:
-          data (str): string with assumed utf-8 encoding
+          data (str or bytes)
         """
-        # First thing is first, make sure this is unicode and not some fake
-        # str with non-ascii characters floating around
-        if not self.isbinary:
-            data = data.decode('utf-8', 'ignore')
-        # See if we have anything left over from previous iteration
-        if self.productBuffer != "":
-            data = self.productBuffer + data
+        # TODO to understand, is this safe from twisted's 'threading'?
+        # write the data to our buffer
+        self.bytes_received += self.productBuffer.write(data)
 
-        tokens = data.split(self.product_end)
+        # see how many products we may have
+        tokens = self.productBuffer.getvalue().split(self.product_end)
         # If length tokens is 1, then we did not find the splitter
+        # print(("len(tokens) is %s, bytes_received is %s"
+        #        ) % (len(tokens), self.bytes_received))
         if len(tokens) == 1:
-            # log.msg("Token not found, len data %s" % (len(data),))
-            self.productBuffer = data
             return
 
         # Everything up until the last one can always go...
         for token in tokens[:-1]:
-            # log.msg("ldmbridge cb product size: %s" % (len(token),))
-            self.cbFunc(token)
+            # print("calling cbFunc()")
+            if self.isbinary:
+                # we send bytes
+                self.reactor.callLater(0, self.cbFunc, token)
+            else:
+                # we send str
+                self.reactor.callLater(0, self.cbFunc,
+                                       token.decode('utf-8', 'ignore'))
         # We have some cruft left over!
-        if tokens[-1] != "":
-            self.productBuffer = tokens[-1]
-        else:
-            self.productBuffer = ""
+        self.productBuffer = BytesIO(tokens[-1])
 
     def connectionLost(self, reason):
+        """Fired when STDIN is closed"""
         raise NotImplementedError
 
     def process_data(self, data):
+        """callback function, either str or bytes depending on isbinary"""
         raise NotImplementedError
 
     def lineReceived(self, line):
-        ''' needless override to make pylint happy '''
+        """needless override to make pylint happy"""
         pass
 
 
 class LDMProductFactory(stdio.StandardIO):
+    """Our Factory"""
 
-    def __init__(self, protocol, **kwargs):
+    def __init__(self, protocol, reactor=None, **kwargs):
         """ constructor with a protocol instance """
-        stdio.StandardIO.__init__(self, protocol, **kwargs)
+        protocol.reactor = reactor
+        if protocol.dedup:
+            reactor.callLater(90, protocol.clean_cache)
+        stdio.StandardIO.__init__(self, protocol, reactor=reactor, **kwargs)
--- pyldm/tests/test_ldmbridge.py
+++ pyldm/tests/test_ldmbridge.py
@@ -1,108 +1,83 @@
-import os
+"""testing via trial
+
+    run `trial` from root directory
+"""
 from twisted.internet.test.reactormixins import ReactorBuilder
 
 from pyldm import ldmbridge
 
+
 def get_file(name):
     ''' Helper function to get the text file contents '''
-    basedir = os.path.dirname(__file__)
+    # basedir = os.path.dirname(__file__)
     fn = "../testdata/%s" % (name,)
-    return open(fn)
+    return open(fn, 'rb')
+
+
+class MyProductIngestor(ldmbridge.LDMProductReceiver):
+    """hacky"""
+    hits = 0
+
+    def process_data(self, data):
+        """Process data"""
+        self.hits = self.hits + 1
+
+    def connectionLost(self, reason):
+        """Stop"""
+        # print("Processed %s bytes" % (self.bytes_received, ))
+        self.reactor.stop()
 
 
 class StdioFilesTests(ReactorBuilder):
 
     def test_nwwsoi(self):
+        """Can we process a file we got from NWWS-OI"""
         reactor = self.buildReactor()
-
-        class MyProductIngestor(ldmbridge.LDMProductReceiver):
-            hits = 0
-                      
-            def process_data(self, buf):
-                self.hits = self.hits + 1
-        
-            def connectionLost(self, reason):
-                reactor.stop()
-        
-
         f = get_file('nwwsoi_example.txt')
-        
+
         ingest = MyProductIngestor(dedup=True)
-        ldmbridge.LDMProductFactory( ingest, 
-                                     stdin=f.fileno(),
-                                     reactor=reactor )
+        ldmbridge.LDMProductFactory(ingest,
+                                    stdin=f.fileno(),
+                                    reactor=reactor)
 
         self.runReactor(reactor)
-        self.assertEqual(ingest.hits, 1)
+        assert ingest.hits == 4
 
     def test_deduplicate(self):
         reactor = self.buildReactor()
-
-        class MyProductIngestor(ldmbridge.LDMProductReceiver):
-            hits = 0
-                      
-            def process_data(self, buf):
-                self.hits = self.hits + 1
-        
-            def connectionLost(self, reason):
-                reactor.stop()
-        
-
         f = get_file('twoprods.txt')
-        
+
         ingest = MyProductIngestor(dedup=True)
-        ldmbridge.LDMProductFactory( ingest, 
-                                     stdin=f.fileno(),
-                                     reactor=reactor )
+        ldmbridge.LDMProductFactory(ingest,
+                                    stdin=f.fileno(),
+                                    reactor=reactor)
 
         self.runReactor(reactor)
-        self.assertEqual(ingest.hits, 1)
-
+        assert ingest.hits == 1
 
     def test_bridge(self):
         reactor = self.buildReactor()
-
-        class MyProductIngestor(ldmbridge.LDMProductReceiver):
-            hits = 0
-                      
-            def process_data(self, buf):
-                self.hits = self.hits + 1
-        
-            def connectionLost(self, reason):
-                reactor.stop()
-        
-
         f = get_file('twoprods.txt')
-        
+
         ingest = MyProductIngestor()
-        ldmbridge.LDMProductFactory( ingest, 
-                                     stdin=f.fileno(),
-                                     reactor=reactor )
+        ldmbridge.LDMProductFactory(ingest,
+                                    stdin=f.fileno(),
+                                    reactor=reactor)
 
         self.runReactor(reactor)
-        self.assertEqual(ingest.hits, 2)
-      
+        assert ingest.hits == 2
+
     def test_binarydata(self):
         reactor = self.buildReactor()
-
-        class MyProductIngestor(ldmbridge.LDMProductReceiver):
-            hits = 0
-                      
-            def process_data(self, buf):
-                self.hits = self.hits + 1
-        
-            def connectionLost(self, reason):
-                reactor.stop()
-        
-
         f = get_file('threeNIDS.txt')
-        
+
         ingest = MyProductIngestor()
-        ldmbridge.LDMProductFactory( ingest, 
-                                     stdin=f.fileno(),
-                                     reactor=reactor )
+        ldmbridge.LDMProductFactory(ingest,
+                                    stdin=f.fileno(),
+                                    reactor=reactor)
 
         self.runReactor(reactor)
-        self.assertEqual(ingest.hits, 3)
-        
+        assert ingest.hits == 3
+
+
 globals().update(StdioFilesTests.makeTestCaseClasses())
